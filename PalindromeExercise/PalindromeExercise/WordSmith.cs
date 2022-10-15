58fb22eceb3c215162d3ba2a4be4574ba840a9c0
Kyle Rigler <kylelrigler@gmail.com>
2022-10-14 21:53:00 -0400

    finished

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public object Test(string word)
+        {
+            char[] charArray = word.ToCharArray();
+            Array.Reverse(charArray);
+            var reversed =new string(charArray);
+            if (reversed == word)
+            {
+                return true;
+            } 
+            return false;
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,9 +6,20 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("deed", true)]
+        [InlineData("rotor", true)]
+        [InlineData("mom", true)]
+        [InlineData("level", true)]
+        [InlineData("civic",true)]
+        [InlineData("racecar", true)]
+
+        public void IsAPalindrome(string word, bool expected)
         {
+            var wordSmith = new WordSmith();
+            var actual = wordSmith.Test(word);
+            Assert.Equal(expected, actual);
+
 
         }
     }
